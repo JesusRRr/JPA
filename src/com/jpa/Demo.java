79f38299c72e09077d8c76bd4185acd6f0408355
JesusRRr <rolonjesus1996@gmail.com>
2020-04-29 18:56:55 -0500

    feature: add hibernate-core dependency and create an entitymanagerfactory and an entitymanager

--- src/com/jpa/Demo.java
+++ src/com/jpa/Demo.java
@@ -1,14 +1,29 @@
 package com.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import com.jpa.model.Person;
 
 public class Demo {
 
 	public static void main(String[] args) {
+		EntityManagerFactory emf= Persistence.createEntityManagerFactory("JPA");
+		EntityManager em=emf.createEntityManager();
+		
 		Person person = new Person();
-		person.setLastName("Jesus");
+		person.setId(2);
+		person.setName("Jesus");
 		person.setLastName("R");
 		person.setAge(23);
+		em.getTransaction().begin();
+		em.persist(person);
+		em.getTransaction().commit();
+		em.close();
+		
+		
 	}
+	
 
 }
--- src/com/jpa/model/Person.java
+++ src/com/jpa/model/Person.java
@@ -1,9 +1,15 @@
 package com.jpa.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Data;
 
+
+@Entity
 @Data
 public class Person {
+	@Id
 	private int id;
 	private String name;
 	private String lastName;
