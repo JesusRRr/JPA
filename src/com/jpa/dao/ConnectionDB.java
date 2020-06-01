a68902d2f232368e2a6407afb168cb66335eb4f3
JesusRRr <rolonjesus1996@gmail.com>
2020-06-01 12:18:19 -0500

    chore: ConnectionDB class created

---
+++ src/com/jpa/dao/ConnectionDB.java
@@ -0,0 +1,20 @@
+package com.jpa.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConnectionDB {
+	private static EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("JPA");
+	private static EntityManager entityManager;
+
+	public static EntityManager getEntityManager(){
+		
+		if(entityManager==null) {
+			entityManager=entityManagerFactory.createEntityManager();
+		}
+		
+		return entityManager;
+	}
+	
+}
--- src/com/jpa/model/Person.java
+++ src/com/jpa/model/Person.java
@@ -14,4 +14,6 @@ public class Person {
 	private String name;
 	private String lastName;
 	private int age;
+	
+	
 }
